cf6713bb407ed6aace4a6d2141b3f19fae66d63d
natasha-mendonca <adm.natashamendonca@gmail.com>
2026-05-18 21:27:50 -0300

    Primeiro commit Trabalho Final Api

---
+++ trabalhoFinalApi/src/test/java/org/serratec/trabalhoFinalApi/TrabalhoFinalApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.trabalhoFinalApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
