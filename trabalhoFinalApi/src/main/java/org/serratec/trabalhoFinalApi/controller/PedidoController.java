fd4c59f2035a17bb347b54c401b6b6808caa8f9a
Pedro Dayer <pedrob.dayer@gmail.com>
2026-05-24 20:21:57 -0300

    feat: implementa DTOs ItemPedidoBuscar e PedidoBuscar para o GET listarPedidos!

--- trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/controller/PedidoController.java
+++ trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/controller/PedidoController.java
@@ -1,36 +1,48 @@
 package org.serratec.trabalhoFinalApi.controller;
 
-import jakarta.validation.Valid;
-import org.serratec.trabalhoFinalApi.model.PedidoCriar;
-import org.serratec.trabalhoFinalApi.service.PedidoService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+    // TIRAR O COMENTARIO GERAL QUANDO O SERVICE DE PEDIDO ESTIVER FUNCIONANDO
 
-import java.util.UUID;
 
-@RestController
-@RequestMapping("/pedido")
-public class PedidoController {
-
-    PedidoService pedidoService;
-
-    public PedidoController(PedidoService pedidoService) {
-        this.pedidoService = pedidoService;
-    }
-
-    @PostMapping
-    ResponseEntity<Void> criar (@RequestBody @Valid PedidoCriar pedidoCriar){
-
-        this.pedidoService.inserirPedido(pedidoCriar);
-        return  ResponseEntity.ok().build();
-    }
-
-//    @GetMapping
-    //ADICIONAR A FUNCIONALIADE PAGABLE
-//    ResponseEntity<PedidoBuscar> buscar(@RequestParam (required = false)UUID id){
+//import jakarta.validation.Valid;
+//import org.serratec.trabalhoFinalApi.model.PedidoBuscar;
+//import org.serratec.trabalhoFinalApi.model.PedidoCriar;
+//import org.serratec.trabalhoFinalApi.service.PedidoService;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
 //
-//        return  ;
+//import java.util.List;
+//import java.util.UUID;
+//
+//@RestController
+//@RequestMapping("/pedido")
+//public class PedidoController {
+//
+//    PedidoService pedidoService;
+//
+//    public PedidoController(PedidoService pedidoService) {
+//        this.pedidoService = pedidoService;
 //    }
-
-
-}
+//
+//    @PostMapping
+//    ResponseEntity<Void> criar (@RequestBody @Valid PedidoCriar pedidoCriar){
+//
+//        this.pedidoService.inserirPedido(pedidoCriar);
+//        return  ResponseEntity.ok().build();
+//    }
+//
+//    @GetMapping("/{id}")
+//    ResponseEntity<List<PedidoBuscar>> listarPedidos(@PathVariable UUID id){
+//        return ResponseEntity.ok().body(this.pedidoService.listarPedidos(id));
+//    }
+//
+//
+//
+////    @GetMapping
+//    //ADICIONAR A FUNCIONALIADE PAGABLE
+////    ResponseEntity<PedidoBuscar> buscar(@RequestParam (required = false)UUID id){
+////
+////        return  ;
+////    }
+//
+//
+//}
--- trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/entity/ItemPedido.java
+++ trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/entity/ItemPedido.java
@@ -1,8 +1,16 @@
 package org.serratec.trabalhoFinalApi.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.serratec.trabalhoFinalApi.model.ItemPedidoSolicitacao;
 
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "item_pedido")
 public class ItemPedido {
@@ -21,6 +29,7 @@ public class ItemPedido {
 
     @ManyToOne
     @JoinColumn(name = "pedido_id")
+    @JsonIgnore
     private Pedido pedido;
 
     @Column
@@ -32,12 +41,17 @@ public class ItemPedido {
     @Column
     private Double desconto;
 
+    @Column(name = "sub_total")
+    private Double subTotal;
+
     public ItemPedido(Pedido pedido, Produto produto, ItemPedidoSolicitacao itemPedidoSolicitacao){
         this.pedido = pedido;
         this.produto = produto;
         this.quantidade = itemPedidoSolicitacao.getQuantidade();
         this.valorVenda = produto.getPreco();
         this.desconto = itemPedidoSolicitacao.getDesconto();
+
+        this.subTotal = (valorVenda * quantidade) - desconto;
     }
 
 }
