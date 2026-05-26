d05f2b9275049cb3b1426d90fbefb59e4212114f
natasha-mendonca <adm.natashamendonca@gmail.com>
2026-05-25 21:04:09 -0300

    Incluido Cliente e Endereço

---
+++ trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/model/ClienteCriar.java
@@ -0,0 +1,33 @@
+package org.serratec.trabalhoFinalApi.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClienteCriar {
+
+    @NotBlank
+    @Size(max = 60)
+    private String nome;
+
+    @NotBlank
+    private String cpf;
+
+    @Size(max=50)
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String telefone;
+
+    @NotBlank
+    private String endereco;
+}
---
+++ trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/model/ViaCepResponseNatasha.java
@@ -0,0 +1,4 @@
+package org.serratec.trabalhoFinalApi.model;
+
+public record ViaCepResponseNatasha(String cep, String logradouro, String bairro, String localidade, String uf, String estado) {
+}
