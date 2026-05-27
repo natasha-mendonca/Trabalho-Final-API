ab308fa1f640c1d4a72d527d615da7c83ef6a7ad
natasha-mendonca <adm.natashamendonca@gmail.com>
2026-05-26 23:41:28 -0300

    correção para possibilitar merge

--- trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/TrabalhoFinalApiApplication.java
+++ trabalhoFinalApi/src/main/java/org/serratec/trabalhoFinalApi/TrabalhoFinalApiApplication.java
@@ -3,11 +3,9 @@ package org.serratec.trabalhoFinalApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
-@EnableScheduling
 public class TrabalhoFinalApiApplication {
 
 	public static void main(String[] args) {
@@ -25,24 +23,24 @@ public class TrabalhoFinalApiApplication {
 				                                                  @@@@@ .@@@ @@@@                                   \s
 				                                                 @@    =  @*  @  @                                  \s
 				                                  @@       @@   @@*-..  @@@%.    .%                                 \s
-				                                  @@@     @@@ @@=%@@@@@@@@.@@@@%%@                                \s
+				                                  @@@     @@@ *@@=%@@@@@@@@.@@@@%*%@                                \s
 				                                  @@@    @@@*  @@. .@@@@    :%@   *:                                \s
 				                                  @#@@   @#@   @@@@*     .:     @@@@                                \s
-				                                  @@@   @@   @#*@@@@@@@@@@@@@@@. @                                \s
+				                                  @*@@   @*@   @#*@@@@@@@@@@@@@@@. @                                \s
 				                                  @**@@ @@=@   @@.......:@@@    :@@@                                \s
 				                                  @@+@@ @%=@   @@@@*+=::.....++%@# @                                \s
-				                                   @#@=%:@@   :@ @@@%%%+=+@@   @                                \s
-				                                   @@@@@@@@#    @  @@@%##*#@@    @                                \s
+				                                   @*#@=%:@@   :@ @@@%%%**+*=+@@   @                                \s
+				                                   @@@@@@@@#    @  @@@%#*#**#@@    @                                \s
 				                                @@       :@+    @@  @*%@%%%@@@    @@                                \s
-				                                @%  =          @@  @@%##*@@     @@:                               \s
+				                                @%  *=          @@  @@%#*#*@@     @@:                               \s
 				                                @*#@@@@@@@@@   .@@  @@#%@@@@      @@#                               \s
-				                              @@@%@@@=@@@@     @@  #@##%@@       @                                \s
+				                              @@@%@@@=*@@@@     @@  #@##*%@@       @                                \s
 				                              @@@@@@@*@@@@         :@@%#%@@                                         \s
 				                              @@@@@%*%%@@@        +:@@@%@@=%                                        \s
-				                                @@%@@@@@@       :@@@%%@+..%                                       \s
-				                                 @@%%@@@      @@@@+%#%@   ::                                      \s
+				                                @@%*@@@@@@       :@@@%%*@+..%                                       \s
+				                                 @@*%%*@@@      @@@@+%#%@   ::                                      \s
 				                                  @@@*#%@@    .: @.+=%#@@:@=@:@                                     \s
-				                                  :@@%*@@@   *..@=%#=@  .=*. @                                    \s
+				                                  :@@%**@@@   *..@*=%#=@  .=*. @                                    \s
 				                                  @ @@@=+@@@@@@@@@@%**@@ =  @@ *                                    \s
 				                                     =-:::++==-.  .            .                                    \s
 				
